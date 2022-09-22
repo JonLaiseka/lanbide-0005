b1cce90409f7dc03f1799967b70357b4a3dd579a
javierlete <javierlete@localhost>
2022-09-22 09:18:03 +0200

    Ficheros con gestión de excepciones

--- MF0967/UF2216/src/com/ipartek/formacion/mf0967/uf2216/Ficheros.java
+++ MF0967/UF2216/src/com/ipartek/formacion/mf0967/uf2216/Ficheros.java
@@ -1,5 +1,6 @@
 package com.ipartek.formacion.mf0967.uf2216;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -8,39 +9,49 @@ import java.util.Scanner;
 
 public class Ficheros {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 //		String fichero = "C:\\trabajos\\nuevos";
 //		
 //		System.out.println(fichero);
-		
+
 		String fichero = "datos.txt";
-		
-		FileWriter fw = new FileWriter(fichero);
-		PrintWriter pw = new PrintWriter(fw);
-		
-		pw.println("Hola desde el nuevo fichero");
-		
-		// pw.flush();
-		
-		pw.close();
-		
-		FileReader fr = new FileReader("laksdjflkasdj");
-//		BufferedReader br = new BufferedReader(fr);
-		
-//		String linea;
-		
-//		while((linea = br.readLine()) != null) {
-//			System.out.println("br:" + linea);
-//		}
-		
-		// Java >= 5
-		Scanner sc = new Scanner(fr);
-		
-		while(sc.hasNext()) {
-			System.out.println("sc:" + sc.nextLine());
+
+		PrintWriter pw = null;
+
+		try {
+
+			FileWriter fw = new FileWriter(fichero);
+			pw = new PrintWriter(fw);
+
+			pw.println("Hola desde el nuevo fichero");
+			// pw.flush();
+		} catch (IOException e) {
+			System.err.println("No se ha podido escribir el fichero");
+		} finally {
+			if (pw != null) {
+				pw.close();
+			}
 		}
-		
-		sc.close();
+
+		// Java >= 7
+
+		try (FileReader fr = new FileReader(fichero);
+				// Java >= 5
+				Scanner sc = new Scanner(fr)) {
+			while (sc.hasNext()) {
+				System.out.println("sc:" + sc.nextLine());
+			}
+
+			// String linea;
+
+			// while((linea = br.readLine()) != null) {
+			// System.out.println("br:" + linea);
+			// }
+		} catch (FileNotFoundException e) {
+			System.err.println("No se ha encontrado el fichero");
+		} catch (IOException e) {
+			System.err.println("No se ha podido leer el fichero");
+		}
 	}
 
 }

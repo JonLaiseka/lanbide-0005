df510ed7b3885cf716b73638580fba6c253fe686
javierlete <javierlete@localhost>
2022-09-30 09:41:59 +0200

    Versión final de Consola con lambdas

--- MF0967/UF2216/src/com/ipartek/formacion/mf0967/uf2216/bibliotecas/Consola.java
+++ MF0967/UF2216/src/com/ipartek/formacion/mf0967/uf2216/bibliotecas/Consola.java
@@ -5,10 +5,11 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.Scanner;
+import java.util.function.Function;
 
 public class Consola {
 	private static final Scanner SC = new Scanner(System.in);
-	
+
 	public static void pl() {
 		System.out.println();
 	}
@@ -16,82 +17,55 @@ public class Consola {
 	public static void pl(Object o) {
 		System.out.println(o);
 	}
-	
+
 	public static void ple(Object o) {
 		System.err.println(o);
 	}
-	
+
 	public static void p(Object o) {
 		System.out.print(o);
 	}
-	
+
 	public static String gString(String mensaje) {
 		p(mensaje + ": ");
 		return SC.nextLine();
 	}
-	
+
 	public static int gInt(String mensaje) {
-		return (int) gObject(mensaje, new Convertible() {
-			@Override
-			public Object convertir(String texto) {
-				return Integer.parseInt(texto);
-			}
-		});
+		return gObject(mensaje, texto -> Integer.parseInt(texto));
 	}
-	
+
 	public static long gLong(String mensaje) {
-		return (long) gObject(mensaje, new Convertible() {
-			
-			@Override
-			public Object convertir(String texto) {
-				return Long.parseLong(texto);
-			}
-		});
+		return gObject(mensaje, texto -> Long.parseLong(texto));
 	}
-	
+
 	public static BigDecimal gBigDecimal(String mensaje) {
-		return (BigDecimal) gObject(mensaje, new Convertible() {
-			
-			@Override
-			public Object convertir(String texto) {
-				return new BigDecimal(texto);
-			}
-		});
+		return gObject(mensaje, texto -> new BigDecimal(texto));
 	}
-	
+
 	public static LocalDate gLocalDate(String mensaje) {
-		return (LocalDate) gObject(mensaje, new Convertible() {
-			
-			@Override
-			public Object convertir(String texto) {
-				return LocalDate.parse(texto, DateTimeFormatter.ISO_DATE);
-			}
-		});
+		return gObject(mensaje, texto -> LocalDate.parse(texto, DateTimeFormatter.ISO_DATE));
 	}
-	
-	public static Object gObject(String mensaje, Convertible c) {
+
+	public static <T> T gObject(String mensaje, Function<String, T> op) {
 		boolean correcto = false;
-		
-		Object o = null;
-		
+
+		T o = null;
+
 		do {
 			try {
 				String texto = gString(mensaje);
-				
-				o = c.convertir(texto);
-				
+
+				o = op.apply(texto);
+
 				correcto = true;
 			} catch (NumberFormatException e) {
 				ple("El número no es válido");
-			} catch(DateTimeParseException e) {
+			} catch (DateTimeParseException e) {
 				ple("La fecha no es válida");
 			}
 		} while (!correcto);
-		
+
 		return o;
 	}
-}
-
-interface Convertible {
-	Object convertir(String texto);
 }

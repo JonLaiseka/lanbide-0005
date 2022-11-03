5b85225dcbd920680157f8835816947f07f19b79
Javier Lete <javier@eskura21.com>
2022-11-03 13:18:29 +0100

    Listener funcionando con anotación

--- MF0967/uf2218ejemplo/src/main/java/com/ipartek/formacion/mf0967/uf2218/listeners/InicioServidorListener.java
+++ MF0967/uf2218ejemplo/src/main/java/com/ipartek/formacion/mf0967/uf2218/listeners/InicioServidorListener.java
@@ -5,7 +5,9 @@ import com.ipartek.formacion.mf0967.uf2218.controladores.Globales;
 
 import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
 
+@WebListener
 public class InicioServidorListener implements ServletContextListener {
 
     public void contextInitialized(ServletContextEvent sce)  { 

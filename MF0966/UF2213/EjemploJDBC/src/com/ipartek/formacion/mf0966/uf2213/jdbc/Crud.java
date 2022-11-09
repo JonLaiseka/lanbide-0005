be0d85726b9afd6813617bb98183eeff16f7b6ba
Javier Lete <javier@eskura21.com>
2022-11-09 10:38:21 +0100

    CRUD completo (Create, Retrieve, Update, Delete)

--- MF0966/UF2213/EjemploJDBC/src/com/ipartek/formacion/mf0966/uf2213/jdbc/Crud.java
+++ MF0966/UF2213/EjemploJDBC/src/com/ipartek/formacion/mf0966/uf2213/jdbc/Crud.java
@@ -3,26 +3,90 @@ package com.ipartek.formacion.mf0966.uf2213.jdbc;
 import java.sql.*;
 
 public class Crud {
+	private static final String url = "jdbc:mysql://localhost:3306/mf0966";
+	private static final String user = "root";
+	private static final String password = "admin";
 
-	public static void main(String[] args) throws SQLException, ClassNotFoundException {
-		String url = "jdbc:mysql://localhost:3306/mf0966";
-		String user = "root";
-		String password = "admin";
-		
-		String sql = "SELECT * FROM usuarios";
-		
-		String driver = "com.mysql.cj.jdbc.Driver";
-		
-		Class.forName(driver);
-		
+	private static final String sql = "SELECT * FROM usuarios";
+	private static final String sqlId = "SELECT * FROM usuarios WHERE id = ?";
+	private static final String sqlInsert = "INSERT INTO usuarios (email, password) VALUES (?, ?)";
+	private static final String sqlUpdate = "UPDATE usuarios SET email=?,password=? WHERE id=?";
+	private static final String sqlDelete = "DELETE FROM usuarios WHERE id=?";
+
+	private static final String driver = "com.mysql.cj.jdbc.Driver";
+
+	public static void main(String[] args) throws SQLException {
+
+		select();
 		Connection con = DriverManager.getConnection(url, user, password);
-		Statement st = con.createStatement();
-		ResultSet rs = st.executeQuery(sql);
-		
-		while(rs.next()) {
+		PreparedStatement pst = con.prepareStatement(sqlId);
+		ResultSet rs;
+
+		Long id = 1L;
+
+		pst.setLong(1, id);
+
+		rs = pst.executeQuery();
+
+		// rs = st.executeQuery(sqlId);
+
+		if (rs.next()) {
 			System.out.printf("%s: %s, %s\n", rs.getLong("id"), rs.getString("email"), rs.getString("password"));
 		}
-		
+
+		pst = con.prepareStatement(sqlInsert);
+
+		String email = "yepa@email.net" + Math.random();
+		String pass = "lalaralalaaaa\\'); DROP TABLE usuarios; -- ";
+
+		pst.setString(1, email);
+		pst.setString(2, pass);
+
+		int numRegMod = pst.executeUpdate();
+
+		System.out.println(numRegMod);
+
+		pst = con.prepareStatement(sqlUpdate);
+
+		id = 8L;
+		email = "prueba@email.net";
+		pass = "Prueba";
+
+		pst.setString(1, email);
+		pst.setString(2, pass);
+		pst.setLong(3, id);
+
+		numRegMod = pst.executeUpdate();
+
+		System.out.println(numRegMod);
+
+		pst = con.prepareStatement(sqlDelete);
+
+		id = 10L;
+
+		pst.setLong(1, id);
+
+		numRegMod = pst.executeUpdate();
+
+		System.out.println(numRegMod);
 	}
 
+	private static void select() {
+		try {
+			Class.forName(driver);
+	
+			try (Connection con = DriverManager.getConnection(url, user, password);
+					PreparedStatement pst = con.prepareStatement(sql);
+					ResultSet rs = pst.executeQuery()) {
+				while (rs.next()) {
+					System.out.printf("%s: %s, %s\n", rs.getLong("id"), rs.getString("email"), rs.getString("password"));
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		} catch(ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
 }
+

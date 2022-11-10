24f1ad1ac710c1773c91c4471f29d06049e4ed15
Javier Lete <javier@eskura21.com>
2022-11-10 11:16:03 +0100

    Ejemplo con Maven para web con mysql

---
+++ MF0966/mf0966/src/main/java/com/ipartek/formacion/mf0966/PruebaServlet.java
@@ -0,0 +1,18 @@
+package com.ipartek.formacion.mf0966;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/prueba")
+public class PruebaServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("prueba.jsp").forward(request, response);
+	}
+
+}

f77fe75391121b5e51a9572c8a901954a9ed55c1
Javier Lete <javier@eskura21.com>
2022-12-14 10:36:25 +0100

    Añadir el DAO empleado a globales

--- MF0966/mf0966ejemplo/src/main/java/com/ipartek/formacion/mf0966ejemplo/controladores/Globales.java
+++ MF0966/mf0966ejemplo/src/main/java/com/ipartek/formacion/mf0966ejemplo/controladores/Globales.java
@@ -2,6 +2,7 @@ package com.ipartek.formacion.mf0966ejemplo.controladores;
 
 import com.ipartek.formacion.mf0966ejemplo.accesodatos.Dao;
 import com.ipartek.formacion.mf0966ejemplo.accesodatos.DaoMySqlCategoria;
+import com.ipartek.formacion.mf0966ejemplo.accesodatos.DaoMySqlEmpleado;
 import com.ipartek.formacion.mf0966ejemplo.accesodatos.DaoMySqlFactura;
 import com.ipartek.formacion.mf0966ejemplo.accesodatos.DaoMySqlProducto;
 import com.ipartek.formacion.mf0966ejemplo.accesodatos.DaoMySqlUsuarios;
@@ -19,6 +20,7 @@ public class Globales {
 	public static final Dao<Producto> DAO_PRODUCTO = DaoMySqlProducto.getInstancia();
 	public static final Dao<Categoria> DAO_CATEGORIA = DaoMySqlCategoria.getInstancia();
 	public static final Dao<Factura> DAO_FACTURA = DaoMySqlFactura.getInstancia();
+	public static final Dao<Empleado> DAO_EMPLEADO = DaoMySqlEmpleado.getInstancia();
 	
 	public static final Empleado EMPLEADO = new Empleado(5L, null, null, null);
 }

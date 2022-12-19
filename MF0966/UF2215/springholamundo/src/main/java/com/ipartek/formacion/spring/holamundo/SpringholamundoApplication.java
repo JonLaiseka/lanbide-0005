c1c19839880cf9349d1391f428cd91c4a0b3dd6b
Javier Lete <javier@eskura21.com>
2022-12-19 09:56:09 +0100

    Proyecto básico con Spring Boot

---
+++ MF0966/UF2215/springholamundo/src/main/java/com/ipartek/formacion/spring/holamundo/SpringholamundoApplication.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.spring.holamundo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringholamundoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringholamundoApplication.class, args);
+	}
+
+}
---
+++ MF0966/UF2215/springholamundo/src/test/java/com/ipartek/formacion/spring/holamundo/SpringholamundoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.spring.holamundo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringholamundoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

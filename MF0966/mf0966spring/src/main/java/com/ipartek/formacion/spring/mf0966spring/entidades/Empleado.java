ecd389d8ecbe716db1fd8a10db58915548f41ec6
Javier Lete <javier@eskura21.com>
2022-12-23 09:31:59 +0100

    Entidades del proyecto anterior

---
+++ MF0966/mf0966spring/src/main/java/com/ipartek/formacion/spring/mf0966spring/entidades/Empleado.java
@@ -0,0 +1,40 @@
+package com.ipartek.formacion.spring.mf0966spring.entidades;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Builder
+public class Empleado {
+	private Long id;
+	private String nombre;
+	private String nif;
+
+	private Empleado jefe;
+
+	public Empleado(Long id, String nombre, String nif, Empleado jefe) {
+		setId(id);
+		setNombre(nombre);
+		setNif(nif);
+		setJefe(jefe);
+	}
+
+	public void setId(Long id) {
+		if (id != null && jefe != null && jefe.getId() != null && jefe.getId() == id) {
+			throw new IllegalArgumentException("Un empleado no puede ser jefe de sí mismo");
+		}
+
+		this.id = id;
+	}
+
+	public void setJefe(Empleado jefe) {
+		if (this.getId() != null && jefe != null && jefe.getId() != null && jefe.getId() == this.getId()) {
+			throw new IllegalArgumentException("Un empleado no puede ser jefe de sí mismo");
+		}
+
+		this.jefe = jefe;
+	}
+
+}

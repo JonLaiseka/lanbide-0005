61a527199d555a18207fc83df0eb7c3eaf83887f
Javier Lete <javier@eskura21.com>
2022-12-23 11:03:26 +0100

    Repositorios REST con Spring Rest Repositories

--- MF0966/mf0966spring/src/main/java/com/ipartek/formacion/spring/mf0966spring/repositorios/EmpleadoRepository.java
+++ MF0966/mf0966spring/src/main/java/com/ipartek/formacion/spring/mf0966spring/repositorios/EmpleadoRepository.java
@@ -2,12 +2,15 @@ package com.ipartek.formacion.spring.mf0966spring.repositorios;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.ipartek.formacion.spring.mf0966spring.entidades.Empleado;
 
-public interface EmpleadoRepository extends CrudRepository<Empleado, Long> {
+@RepositoryRestResource(path = "empleados", collectionResourceRel = "empleados")
+public interface EmpleadoRepository extends PagingAndSortingRepository<Empleado, Long>, CrudRepository<Empleado,Long> {
 	public Empleado findByNombre(String nombre);
 	
-	@Query("from Empleado where nombre = ?")
+	@Query("from Empleado where nombre = :nombre")
 	public Empleado getByNombre(String nombre);
 }

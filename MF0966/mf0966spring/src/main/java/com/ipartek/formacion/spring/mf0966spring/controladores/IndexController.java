556778f670ddd6bec93890d4a555f9604055a80c
Javier Lete <javier@eskura21.com>
2023-01-03 10:19:37 +0100

    Se ve la cantidad del carrito en la lista general de productos

--- MF0966/mf0966spring/src/main/java/com/ipartek/formacion/spring/mf0966spring/controladores/IndexController.java
+++ MF0966/mf0966spring/src/main/java/com/ipartek/formacion/spring/mf0966spring/controladores/IndexController.java
@@ -4,13 +4,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
+import com.ipartek.formacion.spring.mf0966spring.entidades.Pedido;
 import com.ipartek.formacion.spring.mf0966spring.servicios.ProductoService;
 
 @Controller
 @RequestMapping("/")
+@SessionAttributes("carrito")
 public class IndexController {
+	@ModelAttribute("carrito")
+	public Pedido getCarrito() {
+		return new Pedido();
+	}
+	
 	@Autowired
 	private ProductoService servicio;
 	

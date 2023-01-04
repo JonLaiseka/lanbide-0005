598528f3b258f46a6f99c9088c33d28c815d0f36
Javier Lete <javier@eskura21.com>
2023-01-04 08:54:11 +0100

    Refactorización de Index y CarritoController para unificar sesión

--- MF0966/mf0966spring/src/main/java/com/ipartek/formacion/spring/mf0966spring/controladores/IndexController.java
+++ MF0966/mf0966spring/src/main/java/com/ipartek/formacion/spring/mf0966spring/controladores/IndexController.java
@@ -5,10 +5,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import com.ipartek.formacion.spring.mf0966spring.entidades.Pedido;
+import com.ipartek.formacion.spring.mf0966spring.servicios.CarritoService;
 import com.ipartek.formacion.spring.mf0966spring.servicios.ProductoService;
 
 @Controller
@@ -21,11 +25,32 @@ public class IndexController {
 	}
 	
 	@Autowired
-	private ProductoService servicio;
+	private ProductoService productoService;
 	
+	@Autowired
+	private CarritoService carritoService;
+
 	@GetMapping
 	public String index(Model modelo) {
-		modelo.addAttribute("productos", servicio.obtenerTodos());
+		modelo.addAttribute("productos", productoService.obtenerTodos());
 		return "index";
+	}
+
+	@GetMapping("/carrito")
+	public String verCarrito() {
+		return "carrito";
+	}
+	
+	@PostMapping("/carrito")
+	public String agregarProductoACarrito(Long id, Integer cantidad, @SessionAttribute Pedido carrito) {
+		carritoService.guardarProductoEnCarrito(id, cantidad, carrito);
+
+		return "redirect:/carrito";
+	}
+
+	@GetMapping("/cerrar-sesion")
+	public String abandonarCarrito(SessionStatus status) {
+		status.setComplete();
+		return "redirect:/";
 	}
 }

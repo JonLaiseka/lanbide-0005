6d302d08b049b82d09e819a45c4a64d8e8129b5c
Javier Lete <javier@eskura21.com>
2023-01-09 08:53:35 +0100

    Uso de esquema estándar de autenticación en base de datos

--- MF0966/mf0966spring/src/main/java/com/ipartek/formacion/spring/mf0966spring/configuraciones/WebSecurityConfig.java
+++ MF0966/mf0966spring/src/main/java/com/ipartek/formacion/spring/mf0966spring/configuraciones/WebSecurityConfig.java
@@ -1,20 +1,40 @@
 package com.ipartek.formacion.spring.mf0966spring.configuraciones;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+// https://www.baeldung.com/spring-security-jdbc-authentication
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
-
+	@Autowired
+	DataSource dataSource;
+	
+	// PASSWORD ENCODER
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+	    return NoOpPasswordEncoder.getInstance();
+	}
+	
+	// AUTENTICACIÓN
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth)
+	  throws Exception {
+	    auth.jdbcAuthentication().dataSource(dataSource);
+	}
+	
 	// AUTORIZACIÓN
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -31,24 +51,4 @@ public class WebSecurityConfig {
 
         return http.build();
     }
-
-    // AUTENTICACIÓN
-	@Bean
-	UserDetailsService userDetailsService() {
-		UserDetails admin =
-			 User.withDefaultPasswordEncoder()
-				.username("admin")
-				.password("contra")
-				.roles("ADMIN")
-				.build();
-		
-		UserDetails usuario =
-				 User.withDefaultPasswordEncoder()
-					.username("pepe")
-					.password("perez")
-					.roles("USER")
-					.build();
-
-		return new InMemoryUserDetailsManager(admin, usuario);
-	}
 }
